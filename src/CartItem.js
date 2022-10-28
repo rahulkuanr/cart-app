d02049888b58ac8a862c83510748eca1a34a754b
rahulkuanr <rahulkuanr@gmail.com>
2022-10-28 10:45:38 +0530

    add state to cart items

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,32 +1,57 @@
 import React from "react";
 
 class CartItem extends React.Component {
-    render () {
-        return (
-            <div className="cart-item">
-                <div className="left-block">
-                    <img style={styles.image}/>
-                </div>
-                <div className="right-block">
-                    <div style={ {fontSize: 25} }>Phone</div>
-                    <div style={ {color: '#777'} }>Rs 65000</div>
-                    <div style={ {color: '#777'} }>Qty: 1</div>
-                    <div className="cart-item-actions">
-                        {/* Buttons */}
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      price: 65000,
+      title: "Mobile Phone",
+      qty: 1,
+      img: "",
+    };
+  }
+  render() {
+    const {price, title, qty } = this.state;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: "#777" }}>Rs {price}</div>
+          <div style={{ color: "#777" }}>Qty: {qty}</div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img
+              alt="add-item"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+            />
+            <img
+              alt="remove-item"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/512/1828/1828906.png"
+            />
+            <img
+              alt="delete-item"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const styles = {
-    image: {
-        height: 110,
-        width: 110,
-        borderRadius: 4,
-        background: '#ccc'
-    }
-}
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: "#ccc",
+  },
+};
 
 export default CartItem;
